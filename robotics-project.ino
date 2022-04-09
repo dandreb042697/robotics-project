c5cc0298df4339c50ba2a1535c8ca215ddca2b61
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-09 23:38:07 +0800

    Update robotics-project.ino

--- robotics-project.ino
+++ robotics-project.ino
@@ -10,8 +10,12 @@ const int PIN_BUZZER = 5;
 #define MAX_SPEED 150 // sets speed of DC  motors
 #define MAX_SPEED_OFFSET 20
 
-AF_DCMotor leftFront(2);
-AF_DCMotor rightFront(3);
+AF_DCMotor motor2(2, MOTOR1_A);
+// AF_DCMotor motor3(3, MOTOR34_1KHZ);
+
+boolean goesForward = false;
+int distance = 100;
+int speedSet = 0;
 
 Servo servo;
 
@@ -27,152 +31,179 @@ void setup()
     pinMode(PIN_TRIG, OUTPUT);
     pinMode(PIN_ECHO, INPUT);
 
-    leftFront.setSpeed(MAX_SPEED);
-    rightFront.setSpeed(MAX_SPEED);
-
-    // Make sure all wheels are stopped
-    leftFront.run(RELEASE);
-    rightFront.run(RELEASE);
+    ultraSonicDistance = readDistance();
+    delay(100);
+    ultraSonicDistance = readDistance();
+    delay(100);
+    ultraSonicDistance = readDistance();
+    delay(100);
 }
 
 void loop()
 {
+
     int distanceLeft = 0;
     int distanceRight = 0;
+    delay(40);
 
     ultraSonicDistance = lookForward();
 
-    if (ultraSonicDistance < 15)
+    if (ultraSonicDistance <= 15)
     {
+
         moveStop();
-
-        tone(PIN_BUZZER, 1500, 300);
-        delay(500);
-        tone(PIN_BUZZER, 500, 300);
-        delay(500);
-        tone(PIN_BUZZER, 1500, 300);
+        delay(100);
+        moveBackward();
         delay(300);
+        moveStop();
+        delay(200);
 
-        lookRight(); // call this method to avoid zero return value
-        delay(300);
+        playBuzzer();
+
+        // Get distance from left and right
         distanceRight = lookRight();
         delay(300);
-        lookLeft(); // call this method to avoid zero return value
-        delay(300);
         distanceLeft = lookLeft();
+        delay(300);
 
-        if (distanceLeft < 15 && distanceRight < 15)
+        if (distanceRight >= distanceLeft)
         {
-            // move backward
-            moveBackward();
-            delay(1000);
+            turnRight();
+            moveStop();
         }
         else
         {
-            if (distanceLeft > distanceRight)
-            {
-                // move to left
-                Serial.println("Move left");
-                turnLeft();
-            }
-            else
-            {
-                // move to right
-                Serial.println("move right");
-                turnRight();
-            }
+            turnLeft();
+            moveStop();
         }
     }
     else
     {
-        // move forward
-        // Serial.println("move forward");
         moveForward();
     }
-
-    lookForward(); // call this method to avoid zero return value
 }
 
-void moveForward()
+void playBuzzer()
 {
-    leftFront.run(FORWARD);
-    rightFront.run(FORWARD);
+    tone(PIN_BUZZER, 1500, 300);
+    delay(500);
+    // tone(PIN_BUZZER, 500, 300);
+    // delay(500);
+    // tone(PIN_BUZZER, 1500, 300);
+    // delay(300);
 }
 
-void moveStop()
+int readDistance()
 {
-    leftFront.run(RELEASE);
-    rightFront.run(RELEASE);
-}
+    // Clears the trigPin condition
+    digitalWrite(PIN_TRIG, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+    digitalWrite(PIN_TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(PIN_TRIG, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    int duration = pulseIn(PIN_ECHO, HIGH);
 
-void moveBackward()
-{
-    leftFront.run(BACKWARD);
-    rightFront.run(BACKWARD);
-}
-
-void turnLeft()
-{
-    leftFront.run(BACKWARD);
-    rightFront.run(FORWARD);
-    delay(300);
-    moveStop();
-}
-
-void turnRight()
-{
-    leftFront.run(FORWARD);
-    rightFront.run(BACKWARD);
-    delay(300);
-    moveStop();
+    return duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
 }
 
 int lookForward()
 {
     servo.write(90);
+    int value = readDistance();
+    readDistance();
 
-    // Clears the trigPin condition
-    digitalWrite(PIN_TRIG, LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-    digitalWrite(PIN_TRIG, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(PIN_TRIG, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    int duration = pulseIn(PIN_ECHO, HIGH);
-
-    return duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+    return value;
 }
 
 int lookLeft()
 {
     servo.write(180);
-    // Clears the trigPin condition
-    digitalWrite(PIN_TRIG, LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-    digitalWrite(PIN_TRIG, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(PIN_TRIG, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    int duration = pulseIn(PIN_ECHO, HIGH);
-    // Calculating the distance
-    return duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+    int value = readDistance();
+    readDistance();
+    servo.write(90);
+
+    return value;
 }
 
 int lookRight()
 {
     servo.write(0);
 
-    // // Clears the trigPin condition
-    digitalWrite(PIN_TRIG, LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-    digitalWrite(PIN_TRIG, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(PIN_TRIG, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    int duration = pulseIn(PIN_ECHO, HIGH);
-    // Calculating the distance
-    return duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+    int value = readDistance();
+    readDistance();
+    servo.write(90);
+
+    return value;
+}
+
+void moveStop()
+{
+
+    Serial.println("Move Stop");
+
+    motor2.run(RELEASE);
+    // motor3.run(RELEASE);
+}
+
+void moveForward()
+{
+    Serial.println("Move Forward");
+
+    if (!goesForward)
+    {
+        goesForward = true;
+
+        motor2.run(FORWARD);
+        // motor3.run(FORWARD);
+
+        for (speedSet = 0; speedSet < MAX_SPEED; speedSet += 2) // slowly bring the speed up to avoid loading down the batteries too quickly
+        {
+
+            motor2.setSpeed(speedSet);
+            // motor3.setSpeed(speedSet);
+            delay(5);
+        }
+    }
+}
+
+void moveBackward()
+{
+    Serial.println("Move Backward");
+
+    goesForward = false;
+
+    motor2.run(BACKWARD);
+    // motor3.run(BACKWARD);
+
+    for (speedSet = 0; speedSet < MAX_SPEED; speedSet += 2) // slowly bring the speed up to avoid loading down the batteries too quickly
+    {
+
+        motor2.setSpeed(speedSet);
+        // motor3.setSpeed(speedSet);
+        delay(5);
+    }
+}
+
+void turnRight()
+{
+    Serial.println("Turn Right");
+
+    motor2.run(FORWARD);
+    // motor3.run(BACKWARD);
+    delay(500);
+    motor2.run(FORWARD);
+    // motor3.run(FORWARD);
+}
+
+void turnLeft()
+{
+    Serial.println("Turn left");
+
+    motor2.run(BACKWARD);
+    // motor3.run(FORWARD);
+    delay(500);
+    motor2.run(FORWARD);
+    // motor3.run(FORWARD);
 }
