b59653fbdeb3f990fe42223f61427fd33b65dfed
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-09 21:05:08 +0800

    Added motors

--- robotics-project.ino
+++ robotics-project.ino
@@ -1,16 +1,22 @@
-#include <AFMotor.h>
-
 #include <Servo.h>
+#include <AFMotor.h>
 
 const int PIN_SERVO = 10;
 const int PIN_TRIG = A0;
 const int PIN_ECHO = A2;
 const int PIN_BUZZER = 5;
 
+#define MAX_DISTANCE 200
+#define MAX_SPEED 150 // sets speed of DC  motors
+#define MAX_SPEED_OFFSET 20
+
+AF_DCMotor leftFront(2);
+AF_DCMotor rightFront(3);
+
 Servo servo;
 
 long duration;
-int distance; // centimeter
+int ultraSonicDistance; // centimeter
 
 void setup()
 {
@@ -20,6 +26,13 @@ void setup()
 
     pinMode(PIN_TRIG, OUTPUT);
     pinMode(PIN_ECHO, INPUT);
+
+    leftFront.setSpeed(MAX_SPEED);
+    rightFront.setSpeed(MAX_SPEED);
+
+    // Make sure all wheels are stopped
+    leftFront.run(RELEASE);
+    rightFront.run(RELEASE);
 }
 
 void loop()
@@ -27,10 +40,12 @@ void loop()
     int distanceLeft = 0;
     int distanceRight = 0;
 
-    distance = lookForward();
+    ultraSonicDistance = lookForward();
 
-    if (distance < 15)
+    if (ultraSonicDistance < 15)
     {
+        moveStop();
+
         tone(PIN_BUZZER, 1500, 300);
         delay(500);
         tone(PIN_BUZZER, 500, 300);
@@ -49,29 +64,68 @@ void loop()
         if (distanceLeft < 15 && distanceRight < 15)
         {
             // move backward
-        }
-
-        if (distanceLeft > distanceRight)
-        {
-            // move to left
-            Serial.println("Move left");
+            moveBackward();
+            delay(1000);
         }
         else
         {
-            // move to right
-            Serial.println("move right");
+            if (distanceLeft > distanceRight)
+            {
+                // move to left
+                Serial.println("Move left");
+                turnLeft();
+            }
+            else
+            {
+                // move to right
+                Serial.println("move right");
+                turnRight();
+            }
         }
     }
     else
     {
         // move forward
         // Serial.println("move forward");
+        moveForward();
     }
 
     lookForward(); // call this method to avoid zero return value
-
 }
 
+void moveForward()
+{
+    leftFront.run(FORWARD);
+    rightFront.run(FORWARD);
+}
+
+void moveStop()
+{
+    leftFront.run(RELEASE);
+    rightFront.run(RELEASE);
+}
+
+void moveBackward()
+{
+    leftFront.run(BACKWARD);
+    rightFront.run(BACKWARD);
+}
+
+void turnLeft()
+{
+    leftFront.run(BACKWARD);
+    rightFront.run(FORWARD);
+    delay(300);
+    moveStop();
+}
+
+void turnRight()
+{
+    leftFront.run(FORWARD);
+    rightFront.run(BACKWARD);
+    delay(300);
+    moveStop();
+}
 
 int lookForward()
 {
@@ -121,8 +175,4 @@ int lookRight()
     int duration = pulseIn(PIN_ECHO, HIGH);
     // Calculating the distance
     return duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-}
-
-void moveStop()
-{
 }
